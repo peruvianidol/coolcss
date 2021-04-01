67ae123cb980ab422e9010a71ac4107c698aa780
Mike Aparicio <peruvianidol@gmail.com>
2021-04-01 13:49:02 -0500

    Add syntax highlighting

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,11 @@
+const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
+
 module.exports = function (eleventyConfig) {
   eleventyConfig.addWatchTarget("./_src/sass/");
   eleventyConfig.addPassthroughCopy("./_src/css");
   eleventyConfig.addPassthroughCopy("./_src/images");
+
+  eleventyConfig.addPlugin(syntaxHighlight);
 
   return {
     dir: {
